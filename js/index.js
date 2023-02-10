20eb02e50af1de9dbfc2664dfb275dd83b3c5457
TiFAu <turchin.anton2@gmail.com>
2023-02-10 20:36:49 +0200

    First release of the calculator

---
+++ js/index.js
@@ -0,0 +1,164 @@
+
+function roundingFunctionToHundredths (argument) {
+    const argumentRoundedToHundredths = Math.round (argument * 100) / 100;
+    return argumentRoundedToHundredths
+}
+
+function energyPotentialCalculation (apparatusVolume, apparatusBurstPressure, atmospherePressure, adiabaticIndex){
+    const energyPotential = apparatusVolume * (apparatusBurstPressure - atmospherePressure)/(adiabaticIndex - 1);
+    return energyPotential
+}
+
+function tntEquivalentExplosiveCalculation (energyPotential) {
+    let tntEquivalentExplosive = 0.4 * energyPotential * Math.pow (10, 3) / 0.9 / 4200; 
+    return tntEquivalentExplosive
+}
+
+function totalCombustibleGasesReducedToSpecificCombustionEnergyCalculation (energyPotential) {
+    let totalCombustibleGasesReducedToSpecificCombustionEnergy = energyPotential / 4.6 / Math.pow (10, 4)
+    return totalCombustibleGasesReducedToSpecificCombustionEnergy
+}
+
+function relativeEnergyPotentialOfExplosivenessCalculation (totalCombustibleGasesReducedToSpecificCombustionEnergy) {
+    let relativeEnergyPotentialOfExplosiveness = 1/16.534 * Math.pow (totalCombustibleGasesReducedToSpecificCombustionEnergy, 1/3);
+    return relativeEnergyPotentialOfExplosiveness
+}
+
+function blockExplosionCategoryCalculation (relativeEnergyPotentialOfExplosiveness, totalCombustibleGasesReducedToSpecificCombustionEnergy) {
+    let blockExplosionCategory = "III" ;
+    if ( 27 <= relativeEnergyPotentialOfExplosiveness && 2000 <= totalCombustibleGasesReducedToSpecificCombustionEnergy < 5000){blockExplosionCategory = "II"};
+    if ( 37 >= relativeEnergyPotentialOfExplosiveness && totalCombustibleGasesReducedToSpecificCombustionEnergy >= 5000){blockExplosionCategory = "I"};
+    return blockExplosionCategory
+}
+
+function calculationOneFunction (radius, energyPotential, atmospherePressure){
+    const oneValue = radius/(Math.pow(energyPotential/atmospherePressure,(1/3)));
+    return oneValue
+}
+
+function calculationTwoFunction (oneValue){
+    const pressureDrop = Math.pow(Math.E, -1.124-1.66*Math.log(oneValue)+0.26*Math.pow(Math.log(oneValue),2));
+    return pressureDrop
+}
+    
+function calculationOfTheRadiusOfTheShockWaveZone (pressureDrop, energyPotential, atmospherePressure){
+    let zoneRadius = 0.5;
+    let x1 = calculationOneFunction(zoneRadius, energyPotential, atmospherePressure);
+    let dropPressure = calculationTwoFunction (x1) * Math.pow (10,2);
+    while (pressureDrop <= dropPressure && zoneRadius < 105){
+        zoneRadius += 0.01;
+        x1 = calculationOneFunction(zoneRadius, energyPotential, atmospherePressure);
+        dropPressure = calculationTwoFunction (x1)* Math.pow (10,2);
+    }
+    //console.log ( `Радиус = ${zoneRadius}, х1 = ${x1}; dropPressure = ${dropPressure} заданное давление = ${pressureDrop}` )
+    return zoneRadius
+}
+
+function displayingTheResultOfTheCalculationToHtml (resultsObject, elementIntoWhichToInsert, arreyWidths) {
+    let keysOfTheResultingObject = Object.keys(resultsObject);
+    let calculationResults = document.createElement("table");
+    let tableHeader = document.createElement("caption");
+    tableHeader.innerHTML = "2. Calculation results | Результати розрахунку";
+    calculationResults.appendChild(tableHeader);
+    for (y=0; y < (keysOfTheResultingObject.length); y++){
+        if (y>0){
+            var tr = document.createElement("tr");
+            let td = document.createElement("td");
+            td.innerHTML = `2.${keysOfTheResultingObject[y]}.`;
+            td.onmousemove = function(){
+                td.parentElement.style.backgroundColor = "blue";
+            }
+            tr.appendChild(td);
+            for (i=0; i < resultsObject[y].length;i++){
+                if (resultsObject[y][i] !== undefined) {
+                    let td = document.createElement("td");
+                    td.innerHTML = resultsObject[y][i];
+                    if (i==0){td.style.textAlign= "left"};
+                    tr.appendChild(td)
+                    }
+                else {break};
+            };
+        }
+        else{
+            var tr = document.createElement("tr");
+            let th = document.createElement("th");
+            th.innerHTML = "№ п./п.";
+            th.style.width = arreyWidths[y];
+            tr.appendChild(th);
+            for (i=0;i < resultsObject[y].length;i++){
+                let th = document.createElement("th");
+                th.innerHTML = resultsObject[y][i];
+                th.style.width = arreyWidths[i+1];
+                tr.appendChild(th)
+            };
+        };
+        calculationResults.appendChild(tr);
+    };
+    calculationResults.id = "tableResults";                
+    elementIntoWhichToInsert.appendChild(calculationResults);
+    resultsDate.classList.remove('resultsDate');
+}
+
+let arrayOfTableColumnWidths = ["5%", "65%", "10%", "10%", "10%"]; //массив значений ширин столбцов таблиц
+
+calc.onclick = function () {
+    let calculationDate = {
+        initialCalculationData: {
+            apparatusVolume: +apparatusVolume.value,
+            adiabaticIndex: +adiabaticIndex.value,
+            atmospherePressure: +atmospherePressure.value,
+            apparatusBurstPressure: +apparatusBurstPressure.value
+        },
+        givenCalculationResults: {
+            energyPotential: null,
+            tntEquivalentExplosive: null,
+            totalCombustibleGasesReducedToSpecificCombustionEnergy: null,
+            relativeEnergyPotentialOfExplosiveness: null,
+            blockExplosionCategory: null,
+            calculatedRadiiOfShockWaveZones: []
+        }
+    }
+    //calculation main parameters 
+    calculationDate.givenCalculationResults.energyPotential = roundingFunctionToHundredths (energyPotentialCalculation(calculationDate.initialCalculationData.apparatusVolume, calculationDate.initialCalculationData.apparatusBurstPressure, calculationDate.initialCalculationData.atmospherePressure, calculationDate.initialCalculationData.adiabaticIndex));
+    calculationDate.givenCalculationResults.tntEquivalentExplosive = roundingFunctionToHundredths (tntEquivalentExplosiveCalculation (calculationDate.givenCalculationResults.energyPotential));
+    calculationDate.givenCalculationResults.totalCombustibleGasesReducedToSpecificCombustionEnergy = roundingFunctionToHundredths (totalCombustibleGasesReducedToSpecificCombustionEnergyCalculation (calculationDate.givenCalculationResults.energyPotential));
+    calculationDate.givenCalculationResults.relativeEnergyPotentialOfExplosiveness = roundingFunctionToHundredths (relativeEnergyPotentialOfExplosivenessCalculation (calculationDate.givenCalculationResults.totalCombustibleGasesReducedToSpecificCombustionEnergy));
+    calculationDate.givenCalculationResults.blockExplosionCategory = blockExplosionCategoryCalculation (calculationDate.givenCalculationResults.relativeEnergyPotentialOfExplosiveness, calculationDate.givenCalculationResults.totalCombustibleGasesReducedToSpecificCombustionEnergy);
+
+    calculationDate.givenCalculationResults.calculatedRadiiOfShockWaveZones[0] = roundingFunctionToHundredths (calculationOfTheRadiusOfTheShockWaveZone (100,calculationDate.givenCalculationResults.energyPotential, calculationDate.initialCalculationData.atmospherePressure));
+    calculationDate.givenCalculationResults.calculatedRadiiOfShockWaveZones[1] = roundingFunctionToHundredths (calculationOfTheRadiusOfTheShockWaveZone (70,calculationDate.givenCalculationResults.energyPotential, calculationDate.initialCalculationData.atmospherePressure));
+    calculationDate.givenCalculationResults.calculatedRadiiOfShockWaveZones[2] = roundingFunctionToHundredths (calculationOfTheRadiusOfTheShockWaveZone (28,calculationDate.givenCalculationResults.energyPotential, calculationDate.initialCalculationData.atmospherePressure));
+    calculationDate.givenCalculationResults.calculatedRadiiOfShockWaveZones[3] = roundingFunctionToHundredths (calculationOfTheRadiusOfTheShockWaveZone (14,calculationDate.givenCalculationResults.energyPotential, calculationDate.initialCalculationData.atmospherePressure));
+    calculationDate.givenCalculationResults.calculatedRadiiOfShockWaveZones[4] = roundingFunctionToHundredths (calculationOfTheRadiusOfTheShockWaveZone (2,calculationDate.givenCalculationResults.energyPotential, calculationDate.initialCalculationData.atmospherePressure));
+
+    let resultParameters = {
+        0: ["Parameter | Параметер", "Units | Одиниці виміру", "Meaning | Позначення", "Value | Значення"],
+        1: ["Енергетичний потенціал","МДж","Е", calculationDate.givenCalculationResults.energyPotential],
+        2: ["Тратиловий еквівалент","кг","W<sub>T</sub>", calculationDate.givenCalculationResults.tntEquivalentExplosive],
+        3: ["Категорія вибухонебезпечності технологічного блоку","-", "-", calculationDate.givenCalculationResults.blockExplosionCategory],
+        4: ["Радіус руйнування в залежності від розміру надлишкового тиска по занах","-", "-","-"],
+        5: ["Надлишковий тиск 100 кПа, смертельні наслідки, руйнування металевих і бетонних конструкцій","м", "R<sub>1</sub>", calculationDate.givenCalculationResults.calculatedRadiiOfShockWaveZones[0]],
+        6: ["Надлишковий тиск 70 кПа, руйнування металевих і бетонних конструкцій до 50%, важкі травми обслуговуючого персоналу","м", "R<sub>2</sub>", calculationDate.givenCalculationResults.calculatedRadiiOfShockWaveZones[1]],
+        7: ["Надлишковий тиск 28 кПа, руйнування будівель без обрушення, приводить до травм середньої важкості обслуговуючого персоналу","м", "R<sub>3</sub>", calculationDate.givenCalculationResults.calculatedRadiiOfShockWaveZones[2]],
+        8: ["Надлишковий тиск 17 кПа, помірне руйнування перегородок, дверей, рам,  легкі травми обслуговуючого персоналу","м", "R<sub>4</sub>", calculationDate.givenCalculationResults.calculatedRadiiOfShockWaveZones[3]],
+        9: ["Надлишковий тиск 2 кПа, малі пошкодження віконних рам (скла), що може до травмування обслуговуючого персоналу","м", "R<sub>5</sub>", calculationDate.givenCalculationResults.calculatedRadiiOfShockWaveZones[4]],
+        10: ["Версія програми розрахунку","","","v.1.01"],
+        11: ["Експорт звіту до Excel", "", "", "<button id = 'resultToExcel' class = 'calc' >Expor to Excel</button>"]
+    }
+    
+    displayingTheResultOfTheCalculationToHtml (resultParameters, results, arrayOfTableColumnWidths)
+
+    resultToExcel.onclick = (() => { //функция вывода результатов расчета в файл excel
+        const uri = 'data:application/vnd.ms-excel;base64,',
+        template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
+        base64 = (s) => window.btoa(unescape(encodeURIComponent(s))),
+        format = (s, c) => s.replace(/{(\w+)}/g, (m, p) => c[p])
+            return (name) => {
+            const table = document.getElementById ('tableResults')
+            const ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+            window.location.href = uri + base64(format(template, ctx))
+        }
+    })()    
+
+    console.log (calculationDate, resultParameters);
+}
